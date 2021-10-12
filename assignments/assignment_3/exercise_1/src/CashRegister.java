5236add7f19b59c4e0f30c59915bd4d919413557
Adam Tharani <adamtharani@me.com>
2021-10-11 23:46:30 -0400

    Exercise 1
    Signed-off-by: Adam Tharani <adamtharani@me.com>

---
+++ assignments/assignment_3/exercise_1/src/CashRegister.java
@@ -0,0 +1,36 @@
+// Model!!
+public class CashRegister {
+    private Product currentProduct;
+    private Scanner productScanner = new Scanner();
+    private Keyboard inputKeyboard = new Keyboard();
+    private Display monitor = new Display();
+    private TicketPrinter receipt = new TicketPrinter();
+    private ProductDB productDatabase = new ProductDB();
+
+    public Product getCurrentProductInfo(Product product) {
+        return product;
+    }
+
+    public void setCurrentProductUPC(int UPCCode) {
+        if (productScanner.scannedUPCCode(UPCCode) == 0) {
+            monitor.displayText("You have scanned an invalid item, please manually enter the UPCCOde");
+            currentProduct = productDatabase.getProductInfo(inputKeyboard.setUPCCode());
+        }
+        else {
+            currentProduct = productDatabase.getProductInfo(UPCCode);
+        }
+
+    }
+
+    public void update() {
+        monitor.displayText(String.format("MONITOR --- Name: %s, Price: %s, UPCCode: %s", currentProduct.name, currentProduct.price, currentProduct.upc));
+        receipt.displayText(String.format("TICKET PRINTER --- Name: %s, Price: %s, UPCCode: %s", currentProduct.name, currentProduct.price, currentProduct.upc));
+
+    }
+
+
+}
+
+
+
+
---
+++ assignments/assignment_3/exercise_1/src/Keyboard.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Keyboard {
+
+    private Scanner keyboard = new Scanner(System.in);
+    public int setUPCCode() {
+        return keyboard.nextInt();
+    }
+}
